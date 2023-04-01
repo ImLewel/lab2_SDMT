259dcbc5294c095503b0d3714db59772b4b0b455
Lev Pavelko <neroheroes6@gmail.com>
2023-04-01 19:57:31 +0300

    Preparing to the second implementation of linked list

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,17 +1,7 @@
 ﻿namespace ImLewel_LinkedList {
   public class Program {
     static void Main(string[] args) {
-      LinkedList list = new();
-      list.Append("1");
-      list.Append("2");
-      list.Append("3");
-      list.Append("s");
-      list.Append("4");
-      list.Append("s");
-      list.Append("5");
 
-      Console.WriteLine(list.findFirst("s"));
-      Console.WriteLine(list.findLast("s"));
     }
   }
 }
